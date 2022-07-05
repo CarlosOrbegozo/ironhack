c87d9f0cb231c6113a967debe04a3084239c5e92
AzureAD\CarlosOrbegozo <carlos.orbegozo@securitasdirect.es>
2022-07-05 19:39:54 +0200

    added class course

---
+++ src/com/ironhack/classes/Course.java
@@ -0,0 +1,47 @@
+package com.ironhack.classes;
+
+public class Course {
+    private int classId;
+    private int capacity;
+    private int studentCount;
+    private String title;
+
+    public Course(int classId, int capacity, int studentCount, String title) {
+        this.classId = classId;
+        this.capacity = capacity;
+        this.studentCount = studentCount;
+        this.title = title;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getStudentCount() {
+        return studentCount;
+    }
+
+    public void setStudentCount(int studentCount) {
+        this.studentCount = studentCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
